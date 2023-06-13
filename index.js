1c117b7f36feb7c6e939f8aa55ef48d1b6ceffd3
artur-britto <britto.monteiro23@gmail.com>
2023-06-13 05:57:20 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const bodyparser= require('body-parser')
+const bodyparser = require('body-parser')
 
 //configurando body parser
 
-app.use(bodyparser.urlencoded({
-    extended: false}
-))
+app.use(bodyparser.urlencoded({extended: false}))
 
 app.use(bodyparser.json())
 
@@ -19,16 +17,20 @@ app.use(express.static('public'))
 
 //Rota principal
 app.get('/', (req, res) => {
-    res.send('<h1>Página Principal</h1>')   
+    res.render('index.ejs')   
 })
 
 //Rota home
-
-app.get('/', (req, res) => {
-    res.render('index')   
+app.get('/home', (req, res) => {
+    res.send('<h1>Home</h1>')
 })
 
-//Iniciando o servidor
+//Rota para filmes
+app.get('/filmes', (req, res) => {
+    res.render('filmes')
+})
+
+    //Iniciando o servidor
 app.listen(port, ()=> {
     console.log('Servidor Online!')
 })
